b98b7083e6813c086326a819a21f2d559d3f94c4
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-07-22 18:19:18 -0300

    Credentials of the MariaDB

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import mariadb
+import sys
+import configparser
+
+def main():
+    """
+    Load config file with database credentials,
+    Connects to the database and processes data
+    Define credentials to Azure
+    :rtype: object
+    """
+    config = configparser.ConfigParser()
+    config.read('Credentials.ini')
+
+    USER = config["MARIADB"]['USER'] 
+    PASS = config["MARIADB"]['PASS']
+    HOST = config["MARIADB"]['HOST']
+    PORT = config["MARIADB"]['PORT']
+    DB   = config["MARIADB"]['DB']
+
+if __name__ == "__main__":
+    main()
