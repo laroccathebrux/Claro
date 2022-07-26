21879aa46e39fe4d60235f654c677ceaa5d8ded0
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-07-26 13:02:18 -0300

    Configparser

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 import pandas as pd
+import mariadb
+import sys
+import configparser
 
-print("Hello World")
+config = configparser.ConfigParser()
+config.read("credentials.ini")
+
+HOST = config["MARIADB"]["HOST"]
+PORT = config["MARIADB"]["PORT"]
+USER = config["MARIADB"]["USER"]
+PASS = config["MARIADB"]["PASS"]
+DBSE = config["MARIADB"]["DBSE"]
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user=USER,
+        password=PASS,
+        host=HOST,
+        port=PORT,
+        database=DBSE
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
