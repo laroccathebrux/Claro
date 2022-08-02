922065c9a253d585c68ccfb69d7a2773e7afa21f
larocca_thebrux <larocca_thebrux@yahoo.com.br>
2022-08-02 17:48:40 -0300

    Nothing

--- main.py
+++ main.py
@@ -3,28 +3,49 @@ import mariadb
 import sys
 import configparser
 
-config = configparser.ConfigParser()
-config.read("credentials.ini")
+def getData(cur):
+    """
+    cur.execute(
+    "SELECT first_name,last_name FROM employees WHERE first_name=?", 
+    (some_name,))
 
-HOST = config["MARIADB"]["HOST"]
-PORT = config["MARIADB"]["PORT"]
-USER = config["MARIADB"]["USER"]
-PASS = config["MARIADB"]["PASS"]
-DBSE = config["MARIADB"]["DBSE"]
+    for (first_name, last_name) in cur:
+        print(f"First Name: {first_name}, Last Name: {last_name}")
+    """
+    return True
 
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user=USER,
-        password=PASS,
-        host=HOST,
-        port=PORT,
-        database=DBSE
+def main():
+    """
+    Load config file with database credentials,
+    Connects to the database and processes data
+    :rtype: object
+    """
 
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+    config = configparser.ConfigParser()
+    config.read("credentials.ini")
 
-# Get Cursor
-cur = conn.cursor()
+    HOST = config["MARIADB"]["HOST"]
+    PORT = config["MARIADB"]["PORT"]
+    USER = config["MARIADB"]["USER"]
+    PASS = config["MARIADB"]["PASS"]
+    DBSE = config["MARIADB"]["DBSE"]
+
+    # Connect to MariaDB Platform
+    try:
+        conn = mariadb.connect(
+            user     = USER,
+            password = PASS,
+            host     = HOST,
+            port     = PORT,
+            database = DBSE
+
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+
+if __name__ == "__main__":
+    main()
